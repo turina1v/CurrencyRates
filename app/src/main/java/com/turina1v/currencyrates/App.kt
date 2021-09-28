030cc92486787169e81bf75a8aa460529196e1b5
Valentina Tyurina <vktyurin@mts.ru>
2021-09-28 21:54:25 +0300

    Add: register Timber in App

--- app/src/main/java/com/turina1v/currencyrates/App.kt
+++ app/src/main/java/com/turina1v/currencyrates/App.kt
@@ -5,10 +5,16 @@ import com.turina1v.currencyrates.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
         startKoin {
             androidLogger()
             androidContext(this@App)
